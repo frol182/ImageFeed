6bf98fc51ce1762b37614f436c7655f8080aec82
frol182 <frol182@gmail.com>
2023-09-04 14:59:37 +0300

    Сдача 8 спринта

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -10,7 +10,6 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
-    
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var likeButton: UIButton!
